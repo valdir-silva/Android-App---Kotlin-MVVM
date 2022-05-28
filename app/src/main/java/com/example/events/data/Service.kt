71d1a6350a410bdedde3bf62b66e95476792f4a7
valdir-silva <whiteofdark@outlook.com>
2022-05-28 20:53:04 -0300

    abstraindo interface da api e mock

--- app/src/main/java/com/example/events/data/Service.kt
+++ app/src/main/java/com/example/events/data/Service.kt
@@ -2,20 +2,15 @@ package com.example.events.data
 
 import com.example.events.data.response.EventResponse
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Field
 
-interface MockapiServices {
+interface Service {
 
-    @GET("events/")
     fun getEvents(): Call<List<EventResponse>>
 
-    @FormUrlEncoded
-    @POST("checkin/")
     fun checkIn(
         @Field("eventId") eventId: String? = null,
         @Field("name") name: String,
         @Field("email") email: String
     ): Call<Boolean>
-
-//    fun checkIn(@Body checkin: Checkin): Call<EventApiResult>
 }

cf21b06d0418affe61cec2730cfdb405bcbaef49
valdir-silva <whiteofdark@outlook.com>
2022-05-28 20:46:07 -0300

    implementando api

---
+++ app/src/main/java/com/example/events/data/ApiEndpoints.kt
@@ -0,0 +1,24 @@
+package com.example.events.data
+
+import com.example.events.data.response.EventResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiEndpoints : Service {
+
+    @GET("events/")
+    override fun getEvents(): Call<List<EventResponse>>
+
+    @FormUrlEncoded
+    @POST("checkin/")
+    override fun checkIn(
+        @Field("eventId") eventId: String?,
+        @Field("name") name: String,
+        @Field("email") email: String
+    ): Call<Boolean>
+
+//    fun checkIn(@Body checkin: Checkin): Call<EventApiResult>
+}
